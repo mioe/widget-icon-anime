24a2255f5b2468f349a7c49075918d0e0117220e
Misha Gezha <mioe.boss@gmail.com>
2023-04-03 18:38:02 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+	console.log('🦕 vite.config.ts/defineConfig', command, mode)
+
+	return {
+		base: mode === 'development' ? './' : '/playground/',
+
+		resolve: {
+			alias: {
+				'~/': `${path.resolve(__dirname, 'src')}/`,
+			},
+		},
+
+		plugins: [
+			vue(),
+
+			// https://github.com/unocss/unocss
+			Unocss(),
+		],
+	}
+})
